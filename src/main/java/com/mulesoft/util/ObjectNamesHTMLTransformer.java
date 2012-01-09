2709082c6e3b48259b67d5a5dac015de8894d5ba
Jonathan Rico <jonathan_rico@softwarenextdoor.com>
2012-01-08 18:06:50 -0800

    Fixed object selection page

--- src/main/java/com/mulesoft/util/ObjectNamesHTMLTransformer.java
+++ src/main/java/com/mulesoft/util/ObjectNamesHTMLTransformer.java
@@ -16,6 +16,7 @@ public class ObjectNamesHTMLTransformer extends AbstractMessageTransformer{
 		lPrices = lPrices.substring(1,lPrices.length()-1);		
 		String sHTML = "<html>" +
 						"<head>" +
+							"<title>iON - Salesforce Initializer</title>"+
 							"<link rel=stylesheet href=http://twitter.github.com/bootstrap/1.4.0/bootstrap.min.css >" +
 							"<script language=javascript> " +
 								"function combofunction(sel){var value = sel.options[sel.selectedIndex].value;}" +
@@ -25,25 +26,26 @@ public class ObjectNamesHTMLTransformer extends AbstractMessageTransformer{
 						"body { padding-top: 60px;}" +
 					    "</style>" +
 						"<body>" +
-						"<div class=container>" +
-						"<div class=row>" +
-						"<div class=span12>" +
-						"<form action=http://localhost:65082/results method=post>" +
-						"<h1>Results for Account</h1>";
+						"<div class=\"topbar\"><div class=\"fill\"><div class=\"container\"><a class=\"brand\" href=\"#\">iON - Salesforce Initializer</a></div></div></div>"+
+						"<div class=\"container\">" +
+						"<div class=\"row\">" +
+						"<div class=\"span16\">" +
+						"<form action=\"http://localhost:65082/results\" method=\"post\">" +
+						"<h1>Available Objects</h1>";
 		StringTokenizer stringtokenizer = new StringTokenizer(lPrices, ",");
-		sHTML += "<select size=10 name=objectsCombo>";
+		sHTML += "<select size=\"20\" name=\"objectsCombo\" style=\"height:75%;width:100%;font-size:160%; \">";
 		while (stringtokenizer.hasMoreElements()) {
 //			System.out.println(stringtokenizer.nextToken());
 			sHTML+="<option>"+stringtokenizer.nextToken()+"</option>";
 		}	
 		sHTML+="</select><br/>" +
-				"<input type=submit value=Initialize! onclick=combofunction(objectsCombo.value) />" + 
-				"<input type=hidden name=userOrig value=" + msg.getSessionProperty("userOrig") + " />" +
-				"<input type=hidden name=passOrig value=" + msg.getSessionProperty("passOrig") + " />" +
-				"<input type=hidden name=tokenOrig value=" + msg.getSessionProperty("tokenOrig") + " />" +
-				"<input type=hidden name=userDest value=" + msg.getSessionProperty("userDest") + " />" +
-				"<input type=hidden name=passDest value=" + msg.getSessionProperty("passDest") + " />" +
-				"<input type=hidden name=tokenDest value=" + msg.getSessionProperty("tokenDest") + " />" +
+				"<div style=\"padding:10px 0;\"><input type=\"submit\" class=\" btn primary\"  value=\"Initialize!\" onclick=\"combofunction(objectsCombo.value)\" /></div>" + 
+				"<input type=\"hidden\" name=\"userOrig\" value=\"" + msg.getSessionProperty("userOrig") + "\" />" +
+				"<input type=\"hidden\" name=\"passOrig\" value=\"" + msg.getSessionProperty("passOrig") + "\" />" +
+				"<input type=\"hidden\" name=\"tokenOrig\" value=\"" + msg.getSessionProperty("tokenOrig") + "\" />" +
+				"<input type=\"hidden\" name=\"userDest\" value=\"" + msg.getSessionProperty("userDest") + "\" />" +
+				"<input type=\"hidden\" name=\"passDest\" value=\"" + msg.getSessionProperty("passDest") + "\" />" +
+				"<input type=\"hidden\" name=\"tokenDest\" value=\"" + msg.getSessionProperty("tokenDest") + "\" />" +
 				"</form>" +
 				"</div>" +
 				"</div>" +
